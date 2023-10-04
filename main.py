9bae4125814d79c8b9054b7fc9c956b015c6255c
Winter Johnson <riley.scott.johnson@gmail.com>
2023-10-04 10:59:39 -0400

    base commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+with open("data.txt") as f:
+    data = [line.split(',') for line in f.readlines()]
+
+x = []
+y = []
+numTotal = 0
+
+for i, day in enumerate(data):
+    for dose in day:
+        time, num = dose.split(':')
+        numTotal += int(num)
+        x.append(int(time))
+        y.append(30 - i)
+
+plt.plot(x, y, marker=".", linestyle='')
+plt.show()
