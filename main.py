dac7f235fc2e69f60c083ae1e3192cf37cf60da8
Riley Johnson <riley.scott.johnson@gmail.com>
2023-10-08 15:43:08 -0400

    new day gas added

--- main.py
+++ main.py
@@ -5,16 +5,28 @@ import matplotlib.pyplot as plt
 with open("data.txt") as f:
     data = [line.split(',') for line in f.readlines()]
 
-x = []
-y = []
+xCart = []
+yCart = []
+xGas = []
+yGas = []
 numTotal = 0
 
 for i, day in enumerate(data):
     for dose in day:
         time, num = dose.split(':')
+        hit = None
+        if len(num) > 1 and num[1] != '\n':
+            hit = num[1]
+            num = num[0]
+        if hit != None and hit == 'g':
+            xGas.append(int(time))
+            yGas.append(30 - i)
+        else:
+            xCart.append(int(time))
+            yCart.append(30 - i)
         numTotal += int(num)
-        x.append(int(time))
-        y.append(30 - i)
 
-plt.plot(x, y, marker=".", linestyle='')
+
+plt.plot(xGas, yGas, marker=".", linestyle='', color="green")
+plt.plot(xCart, yCart, marker='.', linestyle='', color='red')
 plt.show()
