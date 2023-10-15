ccd69d0f7892cc97d7672bf3541b511c0d83f24f
Riley Johnson <riley.scott.johnson@gmail.com>
2023-10-15 09:38:35 -0400

    weekend update

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ for i, day in enumerate(data):
     for dose in day:
         time, num = dose.split(':')
         hit = None
+        if len(time) < 3:
+            time = int(time)/60*100
+        else:
+            time = int(time[-2:])/60*100 + int(time[:-2])*100
+        print(time)
         if len(num) > 1 and num[1] != '\n':
             hit = num[1]
             num = num[0]
             if hit == 'g':
-                xGas.append(int(time))
+                xGas.append(time)
                 yGas.append(30 - i)
         else:
-            xCart.append(int(time))
+            xCart.append(time)
             yCart.append(30 - i)
         numTotal += int(num)
 
